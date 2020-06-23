15ca53cd4c609e7f73712800dcbc29dcd4a473a0
Chris Hesterman <chrishesterman14@gmail.com>
2020-06-22 18:49:11 -0700

    Implement seeding scripts for all tables.
    Not sure about testing yet.

--- database-sql/seed.js
+++ database-sql/seed.js
@@ -1,10 +1,15 @@
+/* eslint-disable import/newline-after-import */
 /* eslint-disable no-plusplus */
 require('dotenv').config();
-const faker = require('faker');
-// const mysql = require('mysql');
-const db = require('./index.js');
 
-const seedQueryString = 'INSERT INTO traits(trait) VALUES (?)';
+const faker = require('faker');
+const db = require('./index.js');
+const traitsSeedQueryString = 'INSERT INTO traits(trait) VALUES (?)';
+const productIdSeedQueryString = 'INSERT INTO products(product_id) VALUES(?)';
+const productToTraitsSeedQueryString =
+  'INSERT INTO traits_products(trait_id, product_id) VALUES(?,?)';
+
+// general generator for types of words, decided to only use descriptors for the time being
 const generateArray = (array, type) => {
   const word =
     type === 'describe'
@@ -20,29 +25,16 @@ const generateArray = (array, type) => {
   return generateArray(array, type);
 };
 
-const describe = generateArray([], 'describe');
-const noun = generateArray([], 'noun');
-
-const generateStringsFromWords = (array1, array2) => {
-  const traits = [];
-  while (traits.length <= 100) {
-    const random1 = Math.floor(Math.random() * 100);
-    const random2 = Math.floor(Math.random() * 100);
-    const traitString = `${array1[random1]} ${array2[random2]}`;
-
-    if (!traits.includes(traitString)) {
-      traits.push(traitString);
-    }
-  }
-  return traits;
-};
-
-const traitsArray = generateStringsFromWords(describe, noun);
-
-const seedDB = (array) => {
+// insert items from an array into DB
+const seedDB = (array, queryString) => {
   db.connection.query('USE traitsDB', () => {
-    for (let i = 1; i <= array.length; i++) {
-      db.connection.query(seedQueryString, [array[i]], (err) => {
+    for (let i = 0; i < array.length; i++) {
+      let queryParams = [array[i]];
+
+      if (array.length > 100) {
+        queryParams = [array[i][0], array[i][1]];
+      }
+      db.connection.query(queryString, queryParams, (err) => {
         if (err) {
           console.log('Error: ', err);
           return;
@@ -53,4 +45,26 @@ const seedDB = (array) => {
   });
 };
 
-seedDB(describe);
+// create array of product to trait mappings, three traits for every product
+const mapTraitsToProducts = (array) => {
+  const traits = [];
+  for (let j = 1; j <= array.length; j++) {
+    const indexes = [];
+    while (indexes.length < 3) {
+      const randomIndex = Math.ceil(Math.random() * array.length);
+      if (!indexes.includes(randomIndex)) {
+        indexes.push(randomIndex);
+        traits.push([j, array[randomIndex - 1]]);
+      }
+    }
+  }
+  return traits;
+};
+
+const traits = generateArray([], 'describe');
+const productIds = Array.from(Array(101).keys()).slice(1);
+const productsToTraitsArray = mapTraitsToProducts(productIds);
+
+seedDB(traits, traitsSeedQueryString);
+seedDB(productIds, productIdSeedQueryString);
+seedDB(productsToTraitsArray, productToTraitsSeedQueryString);

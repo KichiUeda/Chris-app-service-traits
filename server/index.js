cac0ff6b3b5734c32b466a191330d3d8fbeb7ea5
Chris Hesterman <chrishesterman14@gmail.com>
2020-07-11 17:59:10 -0700

    Implement filter so main product id is not included in traits cards
    also quick text-transform: uppercase for labels

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-loop-func */
 /* eslint-disable no-console */
 // require('dotenv').config();
 const express = require('express');
@@ -13,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/traits/:product_id', (req, res) => {
-  const id = req.params.product_id;
+  const id = +req.params.product_id;
   const traits = fetchers.fetchTraitsForProduct(id);
   traits.then((traitsData) => {
     const traitProducts = traitsData.traits.reduce((acc, trait) => {
@@ -21,43 +22,44 @@ app.get('/traits/:product_id', (req, res) => {
     }, []);
     Promise.all(traitProducts)
       .then((resultsFinal) => {
+        // console.log('-----rFinal', resultsFinal);
         resultsFinal.forEach((result) => {
-          console.log('results', result.products);
-          // if (result.products.indexOf(req.params.product_id) >= 0) {
-          //   console.log('duplicate!');
-          // }
+          if (result.products.indexOf(id) >= 0) {
+            let filteredProducts = result.products.filter((product) => {
+              return product !== id;
+            });
+            result.products = filteredProducts;
+          }
           while (result.products.length < 4) {
             const filler = Math.ceil(Math.random() * 100);
             if (!result.products.includes(filler) && filler !== id) {
               result.products.push(filler);
             }
           }
-          console.log('resultsFinal', resultsFinal);
         });
         axios
           .all(
             resultsFinal.map((result) => {
               const requestArray = encodeURI(JSON.stringify(result.products));
-              const requestURL = `http://127.0.0.1:3000/api/${requestArray}?type=thumbnail`;
+              const requestURL = `http://127.0.0.1:3001/api/${requestArray}?type=thumbnail`;
 
               return axios.get(requestURL);
             })
           )
           .then((resArray) => {
-            let productArray = resArray.map((response) => {
+            const productArray = resArray.map((response) => {
+              // console.log(response.data);
               return response.data;
             });
-            console.log('productArray', productArray);
+            // console.log('productArray', productArray);
             for (var i = 0; i < productArray.length; i++) {
               if (
                 productArray[i].every((item, index) => {
-                  console.log('resultsFinal product', resultsFinal[i].products);
-                  console.log('item', item.product_id);
                   return resultsFinal[i].products.includes(item.product_id);
                 })
               ) {
                 console.log('trait', resultsFinal[i].trait);
-                productArray[i].push(resultsFinal[i].trait.toUpperCase());
+                productArray[i].push(resultsFinal[i].trait);
               }
             }
             console.log('From Micko ', productArray);
@@ -99,40 +101,3 @@ app.get('/traits/products/:trait', (req, res) => {
 });
 
 module.exports = app;
-
-// axios
-//   .get(requestURL)
-//   .then((results) => {
-//     console.log('From Micko', results.data);
-//     res.set({ 'Access-Control-Allow-Origin': '*' });
-//     res.send(results.data);
-//   })
-//   .catch((err) => {
-//     if (err) {
-//       throw err;
-//     }
-//   });
-
-// .then((results) => {
-//   Promise.all(results.traits.map((trait) => {
-//     fetchers.fetchProductsForTrait(trait)
-//    }))
-// .catch((err) => {
-//   if (err) {
-//     res.status(505).send(err, 'Please try again');
-//   }
-// });
-
-// res.set({ 'Access-Control-Allow-Origin': '*' });
-// console.log(resultsFinal);
-// res.send(JSON.stringify(resultsFinal));
-// const imagesNeeded = resultsFinal.reduce((acc, result) => {
-//   return acc.concat(result.products);
-// }, []);
-// console.log('products to Micko', imagesNeeded);
-// for (let i = 0; i < resultsFinal.length; i++) {
-//   const requestArray = encodeURI(JSON.stringify(resultsFinal[i].products));
-//   const requestURL = `http://127.0.0.1:3000/api/${requestArray}?type=thumbnail`;
-//   console.log(requestURL);
-
-// }

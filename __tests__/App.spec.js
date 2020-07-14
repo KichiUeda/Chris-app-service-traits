d8822d1da9498cea7b9be40759439108e19def77
Chris Hesterman <chrishesterman14@gmail.com>
2020-07-13 17:30:55 -0700

    Clean up unused imports from App.spec.js

--- __tests__/App.spec.js
+++ __tests__/App.spec.js
@@ -3,10 +3,8 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import styled from 'styled-components';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import defaultState from '../client/src/defaultState.js';
 import App from '../client/src/components/App.jsx';
 import Carousel from '../client/src/components/Carousel.jsx';
 

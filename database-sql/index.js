efc1026d869ca742c349eacc0c13df900419e03b
Chris Hesterman <chrishesterman14@gmail.com>
2020-07-15 11:14:30 -0700

    Change DB user/password, client request URL

--- database-sql/index.js
+++ database-sql/index.js
@@ -1,19 +1,20 @@
 /* eslint-disable no-console */
 const mysql = require('mysql');
 
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+//   database: 'traitsDB'
+// });
+
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  user: 'root',
+  password: 'traits',
+  port: 3306,
   database: 'traitsDB'
 });
-
-// connection.query('USE traitsDB', (error) => {
-//   if (error) {
-//     throw error;
-//   }
-//   console.log('using traitsDB as id: ', connection.threadId);
-// });
 
 module.exports.connection = connection;

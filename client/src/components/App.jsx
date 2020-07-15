f4c0a676b67c0afda47193d8f7feef68ae3b8785
Chris Hesterman <chrishesterman14@gmail.com>
2020-07-15 15:26:18 -0700

    Update App.jsx
    try relative url

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -48,7 +48,7 @@ class App extends React.Component {
   fetchImages(id) {
     // eslint-disable-next-line react/destructuring-assignment
     // const requestURL = `http://127.0.0.1:3005/traits${id}`;
-    const requestURL = `http://ec2-3-22-98-54.us-east-2.compute.amazonaws.com:3005/traits${id}`;
+    const requestURL = `/traits${id}`;
     // eslint-disable-next-line no-undef
     console.log(requestURL);
     // eslint-disable-next-line no-undef
